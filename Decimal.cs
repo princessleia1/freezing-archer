f74afcd9d3d36d1c83c2d8f943c13c9b1d22796c
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-06-27 12:01:28 +1000

    Add Math.PI

--- Decimal.cs
+++ Decimal.cs
@@ -2,16 +2,17 @@ double a = 1.0;
 double b = 3.0;
 Console.WriteLine(a / b);
 
-// m suffix with math more digits to right of decimal point
+// M suffix with math more digits to right of decimal point
 decimal c = 1.0M;
 decimal d = 3.0M;
 Console.WriteLine(c / d);
 
 decimal min = decimal.MinValue;
 decimal max = decimal.MinValue;
-// type with greater precision
+// Type with greater precision
 Console.WriteLine($"Range of decimal type is {min} to {max}");
 
+// Using Math.PI field
 double radius = 2.50;
 double area = Math.PI * radius * radius;
 Console.WriteLine(area);
