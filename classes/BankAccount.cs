a2e58055a621603d967e8e957b3d88d8f091ad8d
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-07-03 13:48:27 +1000

    Add Classes

---
+++ classes/BankAccount.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace classes
+{
+    public class BankAccount
+    // Define behaviour of class members
+    {
+        // Declare properties of data elements
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance { get; }
+        private static int accountNumberSeed = 1234567890;
+
+        //private List<Transaction> allTransactions = new List<Transaction>();
+
+        // Define methods perform single function of reading names
+        public void MakeDeposit(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note)
+        {
+        }
+
+        public BankAccount(string name, decimal initialBalance)
+        {
+            this.Owner = name;
+            this.Balance = initialBalance;
+            // Increment 10 digit account number with each new account created
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+        }
+    }
+    
+}
---
+++ classes/program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("Princess Leia", 1000);
+            // Console.WriteLine("Hello World!"); 
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+        }
+    }
+}
