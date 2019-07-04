19e2b41cd9070710c86d652620b89d712a026112
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-07-04 13:12:14 +1000

    Add transaction list, acc history

--- classes/BankAccount.cs
+++ classes/BankAccount.cs
@@ -9,28 +9,70 @@ namespace classes
         // Declare properties of data elements
         public string Number { get; }
         public string Owner { get; set; }
-        public decimal Balance { get; }
+        //public decimal Balance { get; }
         private static int accountNumberSeed = 1234567890;
 
-        //private List<Transaction> allTransactions = new List<Transaction>();
+        public decimal Balance
+        {
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransactions)
+                {
+                    balance += item.Amount;
+                }
+
+                return balance;
+            }
+        }
+
+        private List<Transaction> allTransactions = new List<Transaction>();
 
         // Define methods perform single function of reading names
         public void MakeDeposit(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of Deposit must be Positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransactions.Add(deposit);
         }
 
         public void MakeWithdrawal(decimal amount, DateTime date, string note)
         {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of Withdrawal must be Positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not Sufficient Funds for this Withdrawal");
+            }
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransactions.Add(withdrawal);
         }
 
         public BankAccount(string name, decimal initialBalance)
         {
-            this.Owner = name;
-            this.Balance = initialBalance;
-            // Increment 10 digit account number with each new account created
             this.Number = accountNumberSeed.ToString();
             accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial Balance");
+        }
+        // Define method to log all transactions
+        public string GetAccountHistory()
+        {
+            var report = new System.Text.StringBuilder();
+
+            report.AppendLine("Date\tAmount\tNote");
+            foreach (var item in allTransactions)
+            {
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{item.Notes}");
+            }
+
+            return report.ToString();
         }
     }
-    
 }
--- classes/program.cs
+++ classes/program.cs
@@ -9,6 +9,12 @@ namespace classes
             var account = new BankAccount("Princess Leia", 1000);
             // Console.WriteLine("Hello World!"); 
             Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance.");
+            account.MakeWithdrawal(500, DateTime.Now, "House Payment");
+            Console.WriteLine(account.Balance);
+            account.MakeDeposit(100, DateTime.Now, "Income Deposit");
+            Console.WriteLine(account.Balance);
+            // Log all transactions
+            Console.WriteLine(account.GetAccountHistory());
         }
     }
 }
