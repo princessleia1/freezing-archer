83fa53fed1460b3a0fb8ed0b3b612d3eb7ad75db
PrincessLeia1 <Kirstene@LAPTOP-5HUS1HH5>
2019-09-18 14:26:34 +1000

    Create http request to rest service on github

---
+++ rest-client/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net.Http;
+using System.Net.Http.Headers;
+
+namespace WebAPIClient
+{
+    class Program
+    {
+        // First need object to retrieve data using http client for request/ responses
+        private static readonly HttpClient client = new HttpClient();
+        static void Main(string[] args)
+        {
+            // Use the Wait method to block and wait for the task to finish
+            ProcessRepositories().Wait();
+        }
+
+        // Use of method to process to  list all repositories under the dotnet org
+        private static async Task ProcessRepositories()
+        {
+            client.DefaultRequestHeaders.Accept.Clear();
+            client.DefaultRequestHeaders.Accept.Add(
+                new MediaTypeWithQualityHeaderValue("application/vnd.github.v3+json"));
+            client.DefaultRequestHeaders.Add("User-Agent", ".NET Foundation Repository Reporter");
+
+            var stringTask = client.GetStringAsync("https://api.github.com/orgs/dotnet/repos");
+
+            var msg = await stringTask;
+            Console.Write(msg);
+        }
+    }
+}
